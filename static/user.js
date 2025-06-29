d8018b5e4115bc6999903d227e24fa8277845ceb
n1ghtr33x <dima.luts.standoff@gmail.com>
2025-06-29 05:31:48 +0200

    design v2
    prototype

--- static/user.js
+++ static/user.js
@@ -7,11 +7,37 @@ function toggleMenu(btn) {
 }
 
 function closeMenu() {
-  const menu = document.getElementById('mobileMenu');
-  const overlay = document.getElementById('overlay');
-  const btn = document.getElementById('open-menu')
+    const menu = document.getElementById('mobileMenu');
+    const overlay = document.getElementById('overlay');
+    const btn = document.getElementById('open-menu')
 
-  menu.classList.remove('active');
-  btn.classList.remove('active');
-  overlay.classList.remove('active');
+    menu.classList.remove('active');
+    btn.classList.remove('active');
+    overlay.classList.remove('active');
 }
+
+const scrollBtn = document.getElementById("scrollTopBtn");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 200) {
+        scrollBtn.classList.add("show");
+    } else {
+        scrollBtn.classList.remove("show");
+    }
+});
+
+scrollBtn.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
+
+const hireMeBtn = document.getElementById("hireMe");
+
+hireMeBtn.addEventListener("click", () => {
+    window.scrollTo({
+        top: document.body.scrollHeight,
+        behavior: "smooth"
+    })
+})
