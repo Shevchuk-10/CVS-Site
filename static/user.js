7c9c125e6652bcafd6ea5f66ecf6b639d52fd4a4
n1ghtr33x <dima.luts.standoff@gmail.com>
2025-06-29 02:29:52 +0200

    Add overlay for mobile menu and improve menu UX
    Introduced an overlay element that appears when the mobile menu is active to improve user experience and focus. Updated CSS to style the overlay, modified JavaScript to handle overlay activation and menu closing, and adjusted the HTML template to include the overlay and proper button IDs.

--- static/user.js
+++ static/user.js
@@ -1,5 +1,17 @@
 function toggleMenu(btn) {
     const menu = document.getElementById('mobileMenu');
+    const overlay = document.getElementById('overlay');
     menu.classList.toggle('active');
     btn.classList.toggle('active');
+    overlay.classList.toggle('active');
+}
+
+function closeMenu() {
+  const menu = document.getElementById('mobileMenu');
+  const overlay = document.getElementById('overlay');
+  const btn = document.getElementById('open-menu')
+
+  menu.classList.remove('active');
+  btn.classList.remove('active');
+  overlay.classList.remove('active');
 }
