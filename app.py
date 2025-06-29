f1aabd62d65a44da61e3488a24639ed37ecc5d16
Sevchuk-10 <luxdegewry@gmail.com>
2025-06-29 07:18:08 +0200

    Fixe Errors with status code

--- app.py
+++ app.py
@@ -6,8 +6,11 @@ from config import username, password
 from token_manager import create_token, verify_token
 from fastapi.middleware.cors import CORSMiddleware
 from routers import posts, auth
-from fastapi.exceptions import RequestValidationError, HTTPException as StarletteHTTPException
 from starlette.requests import Request
+from fastapi.responses import HTMLResponse
+from starlette.exceptions import HTTPException as StarletteHTTPException
+from fastapi.exceptions import RequestValidationError
+
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
@@ -15,7 +18,7 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Заміни на фронтовий домен
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -58,6 +61,31 @@ def login_post(request: Request, login_username: str = Form(...), login_password
     return templates.TemplateResponse("login.html", {"request": request, "error": "Неверный логин или пароль"})
 
 
+@app.exception_handler(StarletteHTTPException)
+async def http_exception_handler(request: Request, exc: StarletteHTTPException):
+    return templates.TemplateResponse(
+        "404.html",
+        {
+            "request": request,
+            "code_error": exc.status_code,
+            "message": exc.detail if exc.detail else "Щось пішло не так"
+        },
+        status_code=exc.status_code
+    )
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    return templates.TemplateResponse(
+        "404.html",
+        {
+            "request": request,
+            "code_error": 422,
+            "message": "Помилка валідації"
+        },
+        status_code=422
+    )
+
+
 @app.get("/dashboard")
 async def dashboard(request: Request):
     token = request.cookies.get("auth_token")
@@ -78,10 +106,6 @@ def logout():
     response.delete_cookie("auth_token")
     return response
 
-@app.exception_handler(StarletteHTTPException)
-async def http_exception_handler(request: Request, exc: StarletteHTTPException):
-    return templates.TemplateResponse("404.html", {"request": request, "code_error": f'{exc.status_code}'},
-                                      status_code=exc.status_code)
 
 app.include_router(posts.router)
 app.include_router(auth.router)
